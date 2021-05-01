0ce7f77370caadc9e06e5466b10ace4a657b2df1
Gabriele Masciulli <gmasciu@gmail.com>
2021-05-01 16:53:30 +0200

    Added pagination logic
    Getting total number of cryptos to display from server

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
-import React from 'react'
-import Table from './components/Table'
+import React, { useEffect, useState } from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import cryptoService from './services/Crypto'
+import Table from './components/Table'
+import Pagination from './components/Pagination'
 
 function App() {
+  const [cryptos, setCryptos] = useState([])
+  const [cryptosCount, setCount] = useState()
+
+  console.log('Cryptos: ', cryptos)
+  console.log('Cryptos count: ', cryptosCount)
+
+  const getCryptos = () => {
+    cryptoService.getInfo('market_cap_desc', '10', '1').then(initialCryptos => {
+      setCryptos(cryptos.concat(initialCryptos))
+    })
+    cryptoService.getTotNumberOfCryptos().then(count => {
+      setCount(count.tot)
+    })
+  }
+
+  useEffect(getCryptos, [])
+
   return (
     <div>
-      <Table />
+      <div className='screener-wrapper'>
+        <Table cryptos={cryptos} />
+      </div>
+      <Pagination count={cryptosCount} />
     </div>
   )
 }
