6bb054476e1098623eea9e12284f773f661798fa
Gabriele Masciulli <gmasciu@gmail.com>
2021-05-01 16:56:29 +0200

    Added pagination logic

--- src/components/Row.js
+++ src/components/Row.js
@@ -1,20 +1,36 @@
 import React from 'react'
 import EmptyStar from './EmpyStar'
 
-const Row = () => {
+const Row = ({ crypto }) => {
+  const {
+    market_cap_rank,
+    name,
+    current_price,
+    symbol,
+    market_cap,
+    total_volume: volume,
+    circulating_supply,
+    price_change_percentage_24h_in_currency: day_change,
+    price_change_percentage_7d_in_currency: week_change,
+  } = crypto
+
   return (
     <tr>
       <td className='text-center'>
         <EmptyStar />
       </td>
-      <td className='text-center'>1</td>
-      <td className='text-start'>Bitcoin BTC</td>
-      <td className='text-end'>$57,892.28</td>
-      <td className='text-end'>6.52%</td>
-      <td className='text-end'>15.31%</td>
-      <td className='text-end'>$1,086,231,080,186</td>
-      <td className='text-end'>$52,809,603,719</td>
-      <td className='text-end'>18,695,756 BTC</td>
+      <td className='text-center'>{market_cap_rank}</td>
+      <td className='text-start'>
+        {name} , {symbol.toUpperCase()}
+      </td>
+      <td className='text-end'>${current_price.toLocaleString()}</td>
+      <td className='text-end'>{day_change.toFixed(2)}%</td>
+      <td className='text-end'>{week_change.toFixed(2)}%</td>
+      <td className='text-end'>${market_cap.toLocaleString()}</td>
+      <td className='text-end'>${volume.toLocaleString()}</td>
+      <td className='text-end'>
+        {circulating_supply.toLocaleString()}, {symbol.toUpperCase()}
+      </td>
       <td className='text-end'>Last 7 Days</td>
     </tr>
   )
