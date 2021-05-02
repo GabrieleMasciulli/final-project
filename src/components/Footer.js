9a7536db9840a0560c4b4a846ab32f1fc54e6559
Gabriele Masciulli <gmasciu@gmail.com>
2021-05-02 21:42:52 +0200

    Updated footer component
    Now showing QcoinCap logo, and a couple of columns with fake links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,17 +1,47 @@
 import React from 'react'
 import '../wwwroot/css/Footer.css'
 
+const Column = () => {
+  return (
+    <div className='column'>
+      <span>Column 1</span>
+      <ul>
+        <li>
+          <a>Item</a>
+        </li>
+        <li>
+          <a>Item</a>
+        </li>
+        <li>
+          <a>Item</a>
+        </li>
+        <li>
+          <a>Item</a>
+        </li>
+        <li>
+          <a>Item</a>
+        </li>
+      </ul>
+    </div>
+  )
+}
+
 const Footer = ({ crypto }) => {
   return (
-    <div className='footer-wrapper'>
+    <footer className='footer-wrapper'>
       <div className='footer-content'>
-        <div className='footer-left'>Replace with QcoinCap logo.</div>
-        <div className='footer-right'>Some links</div>
+        <div className='footer-left'></div>
+        <div className='footer-right'>
+          <Column />
+          <Column />
+          <Column />
+          <Column />
+        </div>
         <div className='footer-meta'>
           @{new Date().getFullYear()} QcoinCap. All rights reserved.
         </div>
       </div>
-    </div>
+    </footer>
   )
 }
 
