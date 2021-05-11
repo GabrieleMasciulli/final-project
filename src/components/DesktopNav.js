45b9132c6d115fe6bc3164074fbf1f87fd70afe2
Gabriele Masciulli <gmasciu@gmail.com>
2021-05-11 20:04:40 +0200

    Displaying real global market's data
    Data includes total number of cryptos in the platform, total market cap, 24hour market's total volume , upcoming, ended and oncoing initial coin offerings

--- src/components/DesktopNav.js
+++ src/components/DesktopNav.js
@@ -1,43 +1,67 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react'
 import DollarIcon from './DollarIcon'
 import SunIcon from './SunIcon'
 
-const DesktopNav = ({ cryptoCount }) => {
-  return (
-    <div className='desktop'>
-      <div className='upper-info-wrapper'>
-        <div className='upper-info-content'>
-          <div className='left-content'>
-            <span>
-              Cryptos:
-              <a href='/'>{cryptoCount === 0 ? '...' : cryptoCount}</a>
-            </span>
-            <span>
-              Exchanges:
-              <a href='/'>346</a>
-            </span>
-            <span>
-              Market Cap:
-              <a href='/'>$2,271,520,574,504</a>
-            </span>
-            <span>
-              Dominance:
-              <a href='/'>BTC: 46.4% ETH: 17.1%</a>
-            </span>
-          </div>
-          <div className='right-content'>
-            <div>
-              <DollarIcon />
-              USD
+//services
+import statsFormatter from '../services/formatStockData'
+
+const DesktopNav = ({ loading, stats }) => {
+  if (loading) {
+    return ''
+  } else {
+    return (
+      <div className='desktop'>
+        <div className='upper-info-wrapper'>
+          <div className='upper-info-content'>
+            <div className='left-content'>
+              <span>
+                Cryptos:
+                <a href='/'>{stats.active_cryptocurrencies}</a>
+              </span>
+              <span>
+                Market Cap🧢  :
+                <a href='#'>
+                  {statsFormatter.formatStats(
+                    'marketcap',
+                    stats.total_market_cap.usd
+                  )}
+                  🤑
+                </a>
+              </span>
+              <span>
+                24h Vol:
+                <a href='#'>
+                  {statsFormatter.formatStats('volume', stats.total_volume.usd)}
+                </a>
+              </span>
+              <span>
+                Upcoming ICOs:
+                <a href='#'>{stats.ended_icos}</a>
+              </span>
+              <span>
+                Ongoing ICOs:
+                <a href='#'>{stats.ongoing_icos}</a>
+              </span>
+              <span>
+                Ended ICOs:
+                <a href='#'>{stats.ended_icos}</a>
+              </span>
             </div>
-            <div>
-              <SunIcon />
+            <div className='right-content'>
+              <div>
+                <DollarIcon />
+                USD
+              </div>
+              <div>
+                <SunIcon />
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
-  )
+    )
+  }
 }
 
 export default DesktopNav
