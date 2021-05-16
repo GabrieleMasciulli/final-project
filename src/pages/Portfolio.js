457ced9d08e65fd3bd44eae575298374c35933f1
Gabriele Masciulli <gmasciu@gmail.com>
2021-05-16 10:19:25 +0200

    Updated portfolio page

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -5,13 +5,33 @@ import AddTransaction from '../components/portfolioPage/AddTransaction'
 
 const Portfolio = () => {
   const [addTransaction, setAddTransaction] = useState(false)
+  const [searchInput, setSearchInput] = useState('')
 
-  return addTransaction ? (
-    <AddTransaction />
-  ) : (
-    <div className='portfolio-page-wrapper'>
-      <div className='portfolio-page-content'>
-        <TopInfo addTransaction={() => setAddTransaction(!addTransaction)} />
+  const handlecancel = () => {
+    setSearchInput('')
+    setAddTransaction(false)
+  }
+
+  const handleSearchChange = e => {
+    const newSearch = e.target.value
+    setSearchInput(newSearch)
+  }
+
+  return (
+    <div>
+      {addTransaction ? (
+        <AddTransaction
+          cancel={handlecancel}
+          value={searchInput}
+          handleChange={handleSearchChange}
+        />
+      ) : (
+        ''
+      )}
+      <div className='portfolio-page-wrapper'>
+        <div className='portfolio-page-content'>
+          <TopInfo addTransaction={() => setAddTransaction(!addTransaction)} />
+        </div>
       </div>
     </div>
   )
