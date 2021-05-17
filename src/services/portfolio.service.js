a2b4103cd238a4e85a50acce116276febacc3a99
Gabriele Masciulli <gmasciu@gmail.com>
2021-05-17 19:47:31 +0200

    Added portfolio service with method to POST transactions
    the server checks whether a position is already open or not... in the case of an existing position, a new transaction will be added

---
+++ src/services/portfolio.service.js
@@ -0,0 +1,12 @@
+/* eslint-disable import/no-anonymous-default-export */
+import axios from 'axios'
+const baseUrl = '/api/transaction'
+
+const postTransaction = newTransaction => {
+  const request = axios.post(baseUrl, newTransaction)
+  return request.then(response => response.data)
+}
+
+export default {
+  postTransaction,
+}
