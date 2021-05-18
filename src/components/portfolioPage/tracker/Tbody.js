352301c277166115de7ac5c996f5c6a36f2b5dc9
Gabriele Masciulli <gmasciu@gmail.com>
2021-05-18 19:29:56 +0200

    Added tbody of assets component containing all assets

---
+++ src/components/portfolioPage/tracker/Tbody.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Asset from './Asset'
+
+const Tbody = () => {
+  return (
+    <tbody>
+      <Asset />
+      <Asset />
+      <Asset />
+      <Asset />
+    </tbody>
+  )
+}
+
+export default Tbody
