b88fe667a5e5da829b8060b5539bcdec067eba51
Gabriele Masciulli <gmasciu@gmail.com>
2021-05-18 17:16:36 +0200

    Created client side validation for trade inputs
    Checking both date and timestamp, price, and quantity validity

---
+++ src/services/tradeValidation.js
@@ -0,0 +1,40 @@
+/* eslint-disable import/no-anonymous-default-export */
+
+const validatePrice = price => {
+  return price > 0 ? 'valid' : 'invalid'
+}
+
+const validateQuantity = quantity => {
+  return quantity > 0 ? 'valid' : 'invalid'
+}
+
+const validateDate = date => {
+  return date !== '' ? 'valid' : 'invalid'
+}
+
+const validateTimestamp = timestamp => {
+  const currentTimestamp = parseInt(new Date(Date.now()).getTime().toFixed(0))
+  return timestamp < currentTimestamp ? 'valid' : 'invalid'
+}
+
+const validateTrade = (quantity, price, date, timestamp) => {
+  const priceValidity = validatePrice(price)
+  const quantityValidity = validateQuantity(quantity)
+  const dateValidity = validateDate(date)
+  const timestampValidity = validateTimestamp(timestamp)
+
+  return priceValidity === 'valid' &&
+    quantityValidity === 'valid' &&
+    dateValidity === 'valid' &&
+    timestampValidity === 'valid'
+    ? 'valid'
+    : 'invalid'
+}
+
+export default {
+  validateDate,
+  validatePrice,
+  validateQuantity,
+  validateTimestamp,
+  validateTrade,
+}
