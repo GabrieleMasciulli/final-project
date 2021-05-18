dbfbf6f1d86ee5eaae554028069a490f8c666bb5
Gabriele Masciulli <gmasciu@gmail.com>
2021-05-18 17:21:22 +0200

    Separated trading window stylesheet from portfolio's

--- src/components/portfolioPage/trade/Trade.js
+++ src/components/portfolioPage/trade/Trade.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react'
+import '../../../static/css/Trade.css'
 import CloseIcon from '../../designItems/CloseIconV2'
 import Additions from './Additions'
 import Type from './Type'
