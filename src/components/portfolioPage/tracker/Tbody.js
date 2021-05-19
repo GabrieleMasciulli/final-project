234d5a6dbf54cbd05cc75fe39d889ed112952b65
Gabriele Masciulli <gmasciu@gmail.com>
2021-05-19 17:48:59 +0200

    Added loading animation when fetching asstes

--- src/components/portfolioPage/tracker/Tbody.js
+++ src/components/portfolioPage/tracker/Tbody.js
@@ -1,13 +1,21 @@
 import React from 'react'
 import Asset from './Asset'
 
-const Tbody = () => {
+const Tbody = ({ assets }) => {
   return (
     <tbody>
-      <Asset />
-      <Asset />
-      <Asset />
-      <Asset />
+      {assets.map(asset => (
+        <Asset
+          key={asset.coin.coingecko_id}
+          price={asset.last_price}
+          coin={asset.coin}
+          dayChange={asset.percentage_day_change}
+          holdingInCrypto={asset.holding_in_crypto}
+          holdingInCurrency={asset.holding_in_currency}
+          profit={asset.profit_in_currency}
+          profitChange={asset.percentage_profit_change}
+        />
+      ))}
     </tbody>
   )
 }
