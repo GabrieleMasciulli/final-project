1a164620b51c6ae9a4e007611e8e5a29822814c1
Gabriele Masciulli <gmasciu@gmail.com>
2021-05-19 17:49:33 +0200

    Now rendering assets from API ans displaying them client side

--- src/components/portfolioPage/tracker/Tracker.js
+++ src/components/portfolioPage/tracker/Tracker.js
@@ -1,15 +1,29 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import Assets from './Assets'
+import portfolioService from '../../../services/portfolio.service'
+import authService from '../../../services/auth.service'
 
 const Tracker = () => {
-  const getAssets = () => {}
+  const user = authService.getCurrentUser()
+  const [assets, setAssets] = useState([])
+  const [loading, setLoading] = useState(false)
+
+  console.log('Assets: ', assets)
+
+  const getAssets = () => {
+    setLoading(true)
+    portfolioService.getPositions(user.id).then(assets => {
+      setAssets(assets)
+      setLoading(false)
+    })
+  }
 
   useEffect(getAssets, [])
+
   return (
     <div className='tracker-wrapper'>
       <div className='chart-balance-wrapper'></div>
-
-      <Assets />
+      <Assets loading={loading} assets={assets} />
     </div>
   )
 }
