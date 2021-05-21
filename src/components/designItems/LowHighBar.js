91954015a30984f48c95ed5655c92f985ab62edf
Gabriele Masciulli <gmasciu@gmail.com>
2021-05-21 20:23:23 +0200

    Created dynamic bar for daily low/high price

---
+++ src/components/designItems/LowHighBar.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import { faCaretUp } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+const LowHighBar = ({ low, high, current }) => {
+  const high_minus_low = high - low
+  const high_minus_current = high - current
+  const bar_width = 100 - (100 * high_minus_current) / high_minus_low
+  console.log(bar_width)
+  const style = {
+    width: `${bar_width}%`,
+  }
+
+  return (
+    <div className='low-high-bar'>
+      <span className='background-bar'>
+        <span style={style} className='front-bar'>
+          <FontAwesomeIcon icon={faCaretUp} />
+        </span>
+      </span>
+    </div>
+  )
+}
+
+export default LowHighBar
