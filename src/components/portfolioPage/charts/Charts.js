76286748e4ac7d5b1beb4755a7c746c76cbb2133
Gabriele Masciulli <gmasciu@gmail.com>
2021-06-01 18:08:17 +0200

    Created component for pie and line charts with  stats

---
+++ src/components/portfolioPage/charts/Charts.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import ChartSelector from './ChartSelector'
+import PieChart from './Pie'
+
+import {
+  faChartPie,
+  faChartLine,
+  faBalanceScaleRight,
+} from '@fortawesome/free-solid-svg-icons'
+
+const Charts = () => {
+  return (
+    <div className='charts-wrapper'>
+      <div className='chart-selector-wrapper'>
+        <ChartSelector text='Line Chart' icon={faChartLine} />
+        <ChartSelector text='Pie Chart' icon={faChartPie} />
+        <ChartSelector text='Statistics' icon={faBalanceScaleRight} />
+      </div>
+
+      <div className='chart-wrapper'>
+        <PieChart />
+      </div>
+    </div>
+  )
+}
+
+export default Charts
