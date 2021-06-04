c8aadee8b54f5fb851ff7bcc41d452b6fdbd695f
Gabriele Masciulli <gmasciu@gmail.com>
2021-06-04 20:08:44 +0200

    Updated formatting service
    Now formatting data all in one-place

--- src/services/formatStockData.js
+++ src/services/formatStockData.js
@@ -1,45 +1,25 @@
 /* eslint-disable import/no-anonymous-default-export */
 const formatData = (type, value) => {
-  if (value === null) return '?'
-
-  let result = ``
-
-  if (type === 'number') {
-    const number = Math.abs(value)
-    const formattedNumber = formatDecimals(number)
-
-    result = `${formattedNumber}`
-  } else if (type === '$') {
-    const number = Math.abs(value)
-    const formattedNumber = formatDecimals(number)
-
-    result = `$${formattedNumber}`
-  } else if (type === 'profit') {
-    const number = Math.abs(value)
-    const formattedNumber = formatDecimals(number)
-
-    result = value > 0 ? `+ $${formattedNumber}` : `- $${formattedNumber}`
-  } else if (type === '%') {
-    result = `${Math.abs(value.toFixed(2))} %`
-  }
-
-  return result
+  if (value === null || value === undefined) return 'No data'
 }
 
 const formatStats = (type, value) => {
-  let result = ``
+  if (value === null || undefined) return 'No data'
+
   if (type === 'volume' || type === 'marketcap') {
-    result = `$${Math.trunc(value).toLocaleString()}`
+    return `$${Math.trunc(value).toLocaleString()}`
   }
+
   if (type === 'market_dominance') {
-    result = value.toFixed(2)
+    return value.toFixed(2)
   }
-  return result
 }
 
 const getTrend = value => {
   return value >= 0 ? 'text-green' : 'text-red'
 }
+
+// ---------------------------------------------
 
 const formatDecimals = number => {
   if (number < 1) {
@@ -52,9 +32,95 @@ const formatDecimals = number => {
   }
 }
 
+const isNull = val => {
+  return val === null ? true : false
+}
+
+const numberWithFormattedDecimals = val => {
+  if (isNull(val)) return 'No data'
+
+  const number = Math.abs(val)
+  const formattedNumber = formatDecimals(number)
+
+  return `${formattedNumber}`
+}
+
+const noValueIfNull = val => {
+  return isNull(val) ? '' : val
+}
+
+const priceValue = val => {
+  if (isNull(val)) return 'No data'
+
+  const number = Math.abs(val)
+  const formattedNumber = formatDecimals(number)
+
+  return `$${formattedNumber}`
+}
+
+const profitValue = val => {
+  if (isNull(val)) return 'No data'
+
+  const number = Math.abs(val)
+  const formattedNumber = formatDecimals(number)
+
+  return val > 0 ? `+ $${formattedNumber}` : `- $${formattedNumber}`
+}
+
+const percentageValue = val => {
+  return isNull(val) ? 'No data' : `${Math.abs(val.toFixed(2))} %`
+}
+
+const localeStringConverter = val => {
+  return isNull(val) ? 'No data' : val.toLocaleString()
+}
+
+const formatHomepageTable = arr => {
+  const filteredData = arr.map(crypto => {
+    return {
+      ...crypto,
+      symbol: crypto.symbol.toUpperCase(),
+      current_price: priceValue(crypto.current_price),
+      market_cap: priceValue(crypto.market_cap),
+      market_cap_rank: noValueIfNull(crypto.noValueIfNull),
+      fully_diluted_valuation: priceValue(crypto.fully_diluted_valuation),
+      total_volume: priceValue(crypto.total_volume),
+      high_24h: priceValue(crypto.high_24h),
+      low_24h: priceValue(crypto.low_24h),
+      price_change_24h: priceValue(crypto.price_change_24h),
+      price_change_percentage_24h: percentageValue(
+        crypto.price_change_percentage_24h
+      ),
+      market_cap_change_24h: priceValue(crypto.market_cap_change_24h),
+      market_cap_change_percentage_24h: percentageValue(
+        crypto.market_cap_change_percentage_24h
+      ),
+      circulating_supply: localeStringConverter(crypto.circulating_supply),
+      total_supply: localeStringConverter(crypto.total_supply),
+      max_supply: localeStringConverter(crypto.max_supply),
+      ath: priceValue(crypto.ath),
+      ath_change_percentage: percentageValue(crypto.ath_change_percentage),
+      atl: priceValue(crypto.atl),
+      atl_change_percentage: percentageValue(crypto.atl_change_percentage),
+      price_change_percentage_1h_in_currency: percentageValue(
+        crypto.price_change_percentage_1h_in_currency
+      ),
+      price_change_percentage_24h_in_currency: percentageValue(
+        crypto.price_change_percentage_24h_in_currency
+      ),
+      price_change_percentage_7d_in_currency: percentageValue(
+        crypto.price_change_percentage_7d_in_currency
+      ),
+    }
+  })
+
+  return filteredData
+}
+
 export default {
   formatData,
   getTrend,
   formatStats,
   formatDecimals,
+  formatHomepageTable,
 }
