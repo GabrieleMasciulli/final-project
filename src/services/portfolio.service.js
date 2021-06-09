849f91743ab63cf53e89a0dd3d5cd74217bdd610
Gabriele Masciulli <gmasciu@gmail.com>
2021-06-09 21:49:42 +0200

    Using async / await and passing auth token for server-side validation

--- src/services/portfolio.service.js
+++ src/services/portfolio.service.js
@@ -1,26 +1,42 @@
 /* eslint-disable import/no-anonymous-default-export */
 import axios from 'axios'
+import authHeader from './auth-header'
 
-const baseUrl = '/api/transaction'
+const baseUrl = '/api/positions'
 
-const postTransaction = newTransaction => {
-  const request = axios.post(baseUrl, newTransaction)
-  return request.then(response => response.data)
+const postTransaction = async newTransaction => {
+  const response = await axios.post(baseUrl, newTransaction, {
+    headers: authHeader(),
+  })
+  return response.data
 }
 
-const getPositions = user => {
-  const request = axios.get(`${baseUrl}/assets`, { params: { user } })
-  return request.then(response => response.data)
+const getPositions = async () => {
+  const response = await axios.get(`${baseUrl}/analysis`, {
+    headers: authHeader(),
+  })
+  return response.data
 }
 
-const getCurrentBalance = user => {
-  const request = axios.get(`${baseUrl}/balance`, { params: { user } })
-  return request.then(response => response.data)
+const getCurrentBalance = async () => {
+  const response = await axios.get(`${baseUrl}/balance`, {
+    headers: authHeader(),
+  })
+  return response.data
 }
 
-const getPieChartData = user => {
-  const request = axios.get(`${baseUrl}/pie`, { params: { user } })
-  return request.then(response => response.data)
+const getPieChartData = async () => {
+  const response = await axios.get(`${baseUrl}/pie`, {
+    headers: authHeader(),
+  })
+  return response.data
+}
+
+const deleteAsset = async id => {
+  const response = await axios.delete(`${baseUrl}/${id}`, {
+    headers: authHeader(),
+  })
+  return response.data
 }
 
 export default {
@@ -28,4 +44,5 @@ export default {
   getPositions,
   getCurrentBalance,
   getPieChartData,
+  deleteAsset,
 }
