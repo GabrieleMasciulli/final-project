596f07bdc6b09ae1cc23ac1752cf923aee2bd5e2
Gabriele Masciulli <gmasciu@gmail.com>
2021-07-05 12:08:24 +0200

    Moved both pagination and cryptos logic into their own components

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,85 +1,19 @@
-import React, { useEffect, useState } from 'react'
-import SyncLoader from 'react-spinners/SyncLoader'
+import React from 'react'
+// import SyncLoader from 'react-spinners/SyncLoader'
 import TopInfo from '../components/homePage/TopInfo'
 import Table from '../components/homePage/Table'
 import Pagination from '../components/Pagination'
-import cryptoService from '../services/Crypto'
-import formatterService from '../services/formatStockData'
 
-//redux part
-import { useDispatch } from 'react-redux'
-import { initializeGlobalStats } from '../reducers/statsNav'
-
-const Home = ({ cryptoCount }) => {
-  const dispatch = useDispatch()
-
-  const [cryptos, setCryptos] = useState([])
-  const [loading, setLoading] = useState(false)
-  const [pagination, setPagination] = useState({
-    page: 1,
-    rows: 20,
-  })
-  const [maxPages, setMaxPage] = useState()
-
-  // console.log(cryptos)
-  // console.log(globalLoading)
-  // console.log(globalStats)
-
-  const getCryptos = () => {
-    // console.log('Getting cryptos...')
-    setLoading(true)
-    cryptoService
-      .getInfo('market_cap_desc', pagination.rows, pagination.page)
-      .then(data => {
-        const formatHomepageTable = formatterService.formatHomepageTable(data)
-        setCryptos(formatHomepageTable)
-        setLoading(false)
-      })
-  }
-
-  useEffect(getCryptos, [pagination])
-
-  //pagination handling
-  const handleRowsChange = e => {
-    const rowsNumber = e.target.value
-    const newPagination = { ...pagination, rows: rowsNumber, page: 1 }
-    const newMaxPage = Math.ceil(cryptoCount / rowsNumber)
-
-    setPagination(newPagination)
-    setMaxPage(newMaxPage)
-  }
-
-  const handlePageChange = (event, value) => {
-    const newPagination = { ...pagination, page: value }
-    setPagination(newPagination)
-  }
-
-  //redux part
-  useEffect(() => {
-    dispatch(initializeGlobalStats())
-  }, [dispatch])
-
+const Home = () => {
   return (
-    <>
-      {loading ? (
-        <div className='loading-wrapper'>
-          <SyncLoader color={'#2196F3'} loading={loading} size={20} />
-        </div>
-      ) : (
-        <div className='screener-wrapper'>
-          <TopInfo />
-
-          <Table cryptos={cryptos} />
-          <Pagination
-            cryptoCount={cryptoCount}
-            pagination={pagination}
-            count={maxPages || Math.ceil(cryptoCount / 20)}
-            pageChage={handlePageChange}
-            rowsChange={handleRowsChange}
-          />
-        </div>
-      )}
-    </>
+    <div className='screener-wrapper'>
+      {/* <div className='loading-wrapper'>
+        <SyncLoader color={'#2196F3'} loading={loading} size={20} />
+      </div> */}
+      <TopInfo />
+      <Table />
+      <Pagination />
+    </div>
   )
 }
 
