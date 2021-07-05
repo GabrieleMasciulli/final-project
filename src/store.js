dca96053da26f5d7cc8652b52586f4bc3da07366
Gabriele Masciulli <gmasciu@gmail.com>
2021-07-05 12:44:36 +0200

    Added both pagination and auth reducers to the redux store

--- src/store.js
+++ src/store.js
@@ -2,12 +2,17 @@ import { createStore, combineReducers, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
 import { composeWithDevTools } from 'redux-devtools-extension'
 
+// reducers
 import cryptoReducer from './reducers/crypto'
 import statsNavbarReducer from './reducers/statsNav'
+import paginationReducer from './reducers/pagination'
+import authReducer from './reducers/authentication'
 
 const reducer = combineReducers({
   globalStats: statsNavbarReducer,
   cryptos: cryptoReducer,
+  pagination: paginationReducer,
+  auth: authReducer,
 })
 
 const store = createStore(reducer, composeWithDevTools(applyMiddleware(thunk)))
