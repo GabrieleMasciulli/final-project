922d0b465add2f521ca702e2f0cbbb27f11054f0
Gabriele Masciulli <gmasciu@gmail.com>
2021-07-05 17:58:49 +0200

    Created a 'Protected' route which checks if the user is logged in before rendering the requested component

---
+++ src/components/PrivateRoute.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Route, Redirect } from 'react-router-dom'
+import { useSelector } from 'react-redux'
+
+const PrivateRoute = ({ children, ...rest }) => {
+  const { isAuthenticated } = useSelector(state => state.auth)
+
+  return (
+    <Route
+      {...rest}
+      render={() => {
+        return isAuthenticated ? children : <Redirect to='/home' />
+      }}
+    />
+  )
+}
+
+export default PrivateRoute
