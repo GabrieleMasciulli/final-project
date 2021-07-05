d881d999da0d1de891b20b31b990e9abe77272cc
Gabriele Masciulli <gmasciu@gmail.com>
2021-07-05 12:23:54 +0200

    Moved formatting function to the general data formatting module

--- src/services/formatStockData.js
+++ src/services/formatStockData.js
@@ -1,4 +1,23 @@
 /* eslint-disable import/no-anonymous-default-export */
+export const abbreviate_number = (num, fixed) => {
+  if (num === null) {
+    return null
+  } // terminate early
+  if (num === 0) {
+    return '0'
+  } // terminate early
+  fixed = !fixed || fixed < 0 ? 0 : fixed // number of decimal places to show
+  var b = num.toPrecision(2).split('e'), // get power
+    k = b.length === 1 ? 0 : Math.floor(Math.min(b[1].slice(1), 14) / 3), // floor at decimals, ceiling at trillions
+    c =
+      k < 1
+        ? num.toFixed(0 + fixed)
+        : (num / Math.pow(10, k * 3)).toFixed(1 + fixed), // divide by power
+    d = c < 0 ? c : Math.abs(c), // enforce -0 is 0
+    e = d + ['', 'K', 'M', 'B', 'T'][k] // append power
+  return e
+}
+
 const formatDecimals = number => {
   if (number < 1) {
     const decimal_zeros = -Math.floor(Math.log10(number) + 1)
