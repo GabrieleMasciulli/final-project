e8a8abe36bef5b8f6a084e1d1320b6c028c9243e
Gabriele Masciulli <gmasciu@gmail.com>
2021-07-05 18:38:26 +0200

    HAndling auth with redux

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect } from 'react'
 import './static/css/Homepage.css'
 import {
   BrowserRouter as Router,
@@ -6,6 +6,7 @@ import {
   Route,
   Redirect,
 } from 'react-router-dom'
+import PrivateRoute from './components/PrivateRoute'
 import StatsNav from './components/StatsNav'
 import Navbar from './components/navbar/Navbar'
 import Home from './pages/Home'
@@ -14,109 +15,54 @@ import Portfolio from './pages/Portfolio'
 import Footer from './components/Footer'
 import SignUp from './components/auth/SignUp'
 import Login from './components/auth/Login'
-import AuthService from './services/auth.service'
+import { useDispatch, useSelector } from 'react-redux'
+import { initializeGlobalStats } from './reducers/statsNav'
+import { getCryptos } from './reducers/crypto'
 
-function App() {
-  const [globalStats, setGlobalStats] = useState([])
-  const [userAction, setUserAction] = useState({
-    wants_to_login: false,
-    wants_to_signup: false,
-  })
-  const [user, setUser] = useState({
-    isAuthenticated: AuthService.getCurrentUser() ? true : false,
-    data: AuthService.getCurrentUser() || null,
-  })
+const App = () => {
+  const dispatch = useDispatch()
 
-  //user actions concerning authentication
-  const handleSignUpClick = () => {
-    const newUserAction = { wants_to_login: false, wants_to_signup: true }
-    setUserAction(newUserAction)
-  }
+  const { rows, page } = useSelector(state => state.pagination)
 
-  const handleLoginClick = () => {
-    const newUserAction = { wants_to_login: true, wants_to_signup: false }
-    setUserAction(newUserAction)
-  }
+  //redux part
+  useEffect(() => {
+    dispatch(initializeGlobalStats())
+  }, [dispatch])
 
-  const handleAuthCancel = () => {
-    const newUserAction = { wants_to_login: false, wants_to_signup: false }
-    setUserAction(newUserAction)
-  }
-
-  const handleUserLogin = () => {
-    const newUser = {
-      isAuthenticated: true,
-      data: AuthService.getCurrentUser(),
-    }
-    setUser(newUser)
-  }
-
-  const handleLogout = () => {
-    AuthService.logout()
-    const newUser = {
-      isAuthenticated: false,
-      data: null,
-    }
-    setUser(newUser)
-  }
+  useEffect(() => {
+    dispatch(getCryptos())
+  }, [dispatch, rows, page])
 
   return (
-    <>
-      <div className='page-wrapper'>
-        <Router>
-          <StatsNav />
-          <Navbar
-            user={user}
-            loginClick={handleLoginClick}
-            signupClick={handleSignUpClick}
-            logoutClick={handleLogout}
-          />
-          <div className='homepage-separator'></div>
-          <Switch>
-            <Route
-              exact
-              path='/'
-              render={() => {
-                return <Redirect to='/home' />
-              }}
-            />
-            <Route
-              exact
-              path='/home'
-              render={() => {
-                return (
-                  <Home
-                    cryptoCount={parseInt(globalStats.active_cryptocurrencies)}
-                  />
-                )
-              }}
-            />
-            <Route exact path='/detail/:id' component={Detail} />
+    <div className='page-wrapper'>
+      <Router>
+        <StatsNav />
+        <Navbar />
+        <div className='homepage-separator'></div>
+        <Switch>
+          <Route exact path='/'>
+            <Redirect to='/home' />
+          </Route>
 
-            <Route exact path='/portfolio' component={Portfolio} />
-          </Switch>
+          <Route exact path='/home'>
+            <Home />
+          </Route>
 
-          {user.isAuthenticated ? (
-            ''
-          ) : (
-            <>
-              <Login
-                changeToSignup={handleSignUpClick}
-                cancel={handleAuthCancel}
-                visible={userAction.wants_to_login}
-                setUser={handleUserLogin}
-              />
-              <SignUp
-                changeToLogin={handleLoginClick}
-                cancel={handleAuthCancel}
-                visible={userAction.wants_to_signup}
-              />
-            </>
-          )}
-        </Router>
-        <Footer />
-      </div>
-    </>
+          <Route exact path='/detail/:id'>
+            <Detail />
+          </Route>
+
+          <PrivateRoute path='/portfolio'>
+            <Portfolio />
+          </PrivateRoute>
+        </Switch>
+
+        <Login />
+        <SignUp />
+      </Router>
+
+      <Footer />
+    </div>
   )
 }
 
