f865876a1e8dc4ff8aa10976af335955c82123e8
Gabriele Masciulli <gmasciu@gmail.com>
2021-07-06 09:15:33 +0200

    Managing the state with redux during the registration process

--- src/components/auth/SignUp.js
+++ src/components/auth/SignUp.js
@@ -1,121 +1,88 @@
-import React, { useState } from 'react'
+import React from 'react'
 import '../../static/css/Auth.css'
 import MailInput from './MailInput'
 import PasswInput from './PasswInput'
 import UsernameInput from './UsernameInput'
 import SubmitBtn from './SubmitBtn'
 import CloseIcon from '../designItems/CloseIcon'
-import AuthService from '../../services/auth.service'
-import authValidation from '../../services/authValidation'
 import { faCheckCircle } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
-const SignUp = ({ cancel, visible, changeToLogin }) => {
-  const [username, setUsername] = useState('')
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
-  const [errors, setErrors] = useState({
-    email: null,
-    username: null,
-    password: null,
-  })
-  const [message, setMessage] = useState('')
-  const [successful, setSuccessful] = useState(false)
+import { useSelector, useDispatch } from 'react-redux'
+import {
+  cancelAuth,
+  showLogin,
+  changeMail,
+  changePassword,
+  changeUsername,
+  registerRequest,
+} from '../../reducers/authentication'
 
-  const onChangeEmail = e => {
-    const email = e.target.value
-    setEmail(email)
-  }
+const SignUp = () => {
+  const dispatch = useDispatch()
 
-  const onChangeUsername = e => {
-    const username = e.target.value
-    setUsername(username)
-  }
-
-  const onChangePassword = e => {
-    const password = e.target.value
-    setPassword(password)
-  }
-
-  const handleSigup = e => {
-    setMessage('')
-
-    const newErrors = {
-      email: authValidation.validateEmail(email),
-      username: authValidation.validateUsername(username),
-      password: authValidation.validatePassword(password),
-    }
-    setErrors(newErrors)
-    const validationResult = authValidation.validateRegistration(
-      email,
-      username,
-      password
-    )
-    if (validationResult === 'valid') {
-      AuthService.register(email, username, password).then(
-        response => {
-          console.log(response)
-          setMessage(response.message)
-          setSuccessful(true)
-        },
-        error => {
-          const resMessage =
-            (error.response &&
-              error.response.data &&
-              error.response.data.message) ||
-            error.message ||
-            error.toString()
-          setMessage(resMessage)
-          setSuccessful(false)
-        }
-      )
-    }
-  }
+  const {
+    failureMessage,
+    successMessage,
+    username,
+    password,
+    email,
+    isSigningUp: visible,
+  } = useSelector(state => state.auth)
 
   const contentStyle = {
     width: '480px',
     height: 'auto',
   }
-  const type = 'signup'
 
   return (
     <div className={`auth-wrapper ${visible ? 'opened' : ''}`}>
       <div className='auth-content' style={contentStyle}>
-        <CloseIcon onClick={cancel} />
-        {!successful ? (
+        <CloseIcon onClick={() => dispatch(cancelAuth())} />
+        {!successMessage ? (
           <div className='form-wrapper'>
             <div className='title'>Create an account</div>
             <div className='top-info'>
               Already have an account?
-              <span onClick={changeToLogin} className='redirect-link'>
+              <span
+                onClick={() => dispatch(showLogin())}
+                className='redirect-link'
+              >
                 Log In
               </span>
             </div>
 
             <MailInput
-              value={email}
-              onChange={onChangeEmail}
-              error={errors.email}
+              email={email}
+              onChange={({ target }) => dispatch(changeMail(target.value))}
             />
             <UsernameInput
-              value={username}
-              onChange={onChangeUsername}
-              error={errors.username}
+              username={username}
+              onChange={({ target }) => dispatch(changeUsername(target.value))}
             />
             <PasswInput
-              value={password}
-              onChange={onChangePassword}
-              error={errors.password}
+              password={password}
+              onChange={({ target }) => dispatch(changePassword(target.value))}
             />
 
-            <SubmitBtn handleClick={handleSigup} type={type} />
+            <SubmitBtn
+              handleClick={() =>
+                dispatch(registerRequest(email, password, username))
+              }
+              type={'signup'}
+            />
+
+            <div className='error-message text-center'>{failureMessage}</div>
           </div>
         ) : (
           <div className='form-wrapper'>
             <div className='success'>
               <FontAwesomeIcon icon={faCheckCircle} />
-              {message}
-              <p onClick={changeToLogin} className='redirect-link'>
+              {successMessage}
+              <p
+                onClick={() => dispatch(showLogin())}
+                className='redirect-link'
+              >
                 Click to Log In
               </p>
             </div>
