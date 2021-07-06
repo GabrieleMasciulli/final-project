5ae70fe359b399ba04b9c432be81a500384def46
Gabriele Masciulli <gmasciu@gmail.com>
2021-07-06 12:00:23 +0200

    Implemented crypto search reducer in the application

--- src/components/navbar/SearchBar.js
+++ src/components/navbar/SearchBar.js
@@ -1,49 +1,28 @@
-import React, { useState, useEffect } from 'react'
+import React, { useEffect } from 'react'
 import Tippy from '@tippyjs/react'
 import 'tippy.js/dist/tippy.css'
 import SearchDropdown from './SearchDropdown'
-import searchService from '../../services/search.service'
 import { faSearch } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { useSelector, useDispatch } from 'react-redux'
+import {
+  changeQuery,
+  initResults,
+  getResults,
+} from '../../reducers/searchCrypto'
 
 const SearchBar = () => {
-  const [searchInput, setSearchInput] = useState('')
-  const [results, setResults] = useState([])
-  const duplicates = new Set()
+  const dispatch = useDispatch()
 
-  // console.log(results)
+  const { query, results } = useSelector(state => state.search)
 
-  const getFirstResultsHook = () => {
-    searchService.getFirst(15).then(results => {
-      setResults(results)
-    })
-  }
-  useEffect(getFirstResultsHook, [])
+  useEffect(() => {
+    if (query === '') dispatch(initResults())
+  }, [dispatch, query])
 
-  const getFilteredResultsHook = () => {
-    if (searchInput.length >= 3) {
-      searchService.getResults(searchInput).then(data => {
-        const filteredResults = data.filter(result => {
-          const duplicate = duplicates.has(result.coingecko_id)
-          duplicates.add(result.coingecko_id)
-          return !duplicate
-        })
-        setResults(filteredResults)
-      })
-    } else if (searchInput.length === 0) {
-      searchService.getFirst(10).then(results => {
-        setResults(results)
-      })
-    }
-  }
-
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  useEffect(getFilteredResultsHook, [searchInput])
-
-  const handleSearchChange = e => {
-    const newSearch = e.target.value
-    setSearchInput(newSearch)
-  }
+  useEffect(() => {
+    if (query !== '') dispatch(getResults(query))
+  }, [dispatch, query])
 
   return (
     <Tippy
@@ -58,8 +37,8 @@ const SearchBar = () => {
       <div className='searchbar-wrapper' aria-expanded='true'>
         <FontAwesomeIcon icon={faSearch} />
         <input
-          onChange={handleSearchChange}
-          value={searchInput}
+          onChange={({ target }) => dispatch(changeQuery(target.value))}
+          value={query}
           className='search'
           placeholder='Search'
         />
--- src/store.js
+++ src/store.js
@@ -7,12 +7,14 @@ import cryptoReducer from './reducers/crypto'
 import statsNavbarReducer from './reducers/statsNav'
 import paginationReducer from './reducers/pagination'
 import authReducer from './reducers/authentication'
+import searchReducer from './reducers/searchCrypto'
 
 const reducer = combineReducers({
   globalStats: statsNavbarReducer,
   cryptos: cryptoReducer,
   pagination: paginationReducer,
   auth: authReducer,
+  search: searchReducer,
 })
 
 const store = createStore(reducer, composeWithDevTools(applyMiddleware(thunk)))
