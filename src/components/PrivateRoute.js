a9a1b298ddf2e83c58c4f9fbd42b90e83eb8b75c
Gabriele Masciulli <gmasciu@gmail.com>
2021-07-07 16:44:22 +0200

    Fixed bug on private route component

--- src/components/PrivateRoute.js
+++ src/components/PrivateRoute.js
@@ -1,15 +1,11 @@
 import React from 'react'
-import { Route, Redirect } from 'react-router-dom'
+import { Route } from 'react-router-dom'
 import { useSelector } from 'react-redux'
 
 const PrivateRoute = ({ children, ...rest }) => {
   const { isAuthenticated } = useSelector(state => state.auth)
 
-  return isAuthenticated ? (
-    <Route {...rest} render={() => children} />
-  ) : (
-    <Redirect to='/home' />
-  )
+  return isAuthenticated && <Route {...rest} render={() => children} />
 }
 
 export default PrivateRoute
