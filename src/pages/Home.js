5e44d823df9c319e66987adba220bbf147a98391
Gabriele Masciulli <gmasciu@gmail.com>
2021-07-10 11:49:40 +0200

    Added loading animation when fetching cryptos from the API

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,17 +1,23 @@
 import React from 'react'
-// import SyncLoader from 'react-spinners/SyncLoader'
+import SyncLoader from 'react-spinners/SyncLoader'
 import TopInfo from '../components/homePage/TopInfo'
 import Table from '../components/homePage/Table'
 import Pagination from '../components/Pagination'
+import { useSelector } from 'react-redux'
 
 const Home = () => {
+  const cryptos = useSelector(state => state.cryptos)
+
   return (
     <div className='screener-wrapper'>
-      {/* <div className='loading-wrapper'>
-        <SyncLoader color={'#2196F3'} loading={loading} size={20} />
-      </div> */}
       <TopInfo />
+
       <Table />
+      {cryptos.length === 0 && (
+        <div className='loading-wrapper'>
+          <SyncLoader color={'#2196F3'} loading={true} size={20} />
+        </div>
+      )}
       <Pagination />
     </div>
   )
